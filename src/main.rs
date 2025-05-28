da6c63cd29d4b637ebad0ee84a1fdc54736e2d96
Daniel Maslowski <info@orangecms.org>
2025-05-28 17:29:47 +0200

    WIP: write ELF

--- src/main.rs
+++ src/main.rs
@@ -45,8 +45,77 @@ struct Aout {
     pc_size: U32,           /* pc/line number table */
 }
 
+// https://en.wikipedia.org/wiki/Executable_and_Linkable_Format
+// https://refspecs.linuxfoundation.org/elf/gabi4+/ch4.eheader.html
+#[derive(FromBytes, Immutable, IntoBytes, Clone, Copy, Debug)]
+#[repr(C, packed)]
+struct Elf {
+    magic: [u8; 4],
+    class: u8,
+    data: u8,       // endianness
+    id_version: u8, //
+    os_abi: u8,
+    abi_version: u8,
+    _res: [u8; 7],
+    elf_type: u16,
+    machine: u16,
+    version: u32,
+    entry: u32,
+    program_header_offset: u32,
+    section_header_offset: u32,
+    flags: u32,
+    elf_header_size: u16,
+    program_header_entry_size: u16,
+    program_header_entry_count: u16,
+    section_header_entry_size: u16,
+    section_header_entry_count: u16,
+    section_header_index_entry: u16,
+}
+
+const AOUT_HEADER_SIZE: usize = 32;
+
+const ELF_HEADER_SIZE: u32 = std::mem::size_of::<Elf>() as u32;
+
+fn aout_mach_to_elf(aout: &Aout) -> u16 {
+    let m = aout.magic;
+    match m {
+        0x978a_0000 => 0x3e, // AMD x64
+        _ => todo!("Architecture not yet supported: {m:08x}"),
+    }
+}
+
 fn aout_to_elf(d: &[u8]) -> Result<Vec<u8>, String> {
-    todo!()
+    if let Ok((aout, _)) = Aout::read_from_prefix(d) {
+        let elf = Elf {
+            magic: [0x7f, b'E', b'L', b'F'],
+            class: 1,      // 32-bit
+            data: 1,       // little endian
+            id_version: 1, // fixed
+            os_abi: 0,
+            abi_version: 0,
+            _res: [0, 0, 0, 0, 0, 0, 0],
+            elf_type: 0x2, // executable
+            machine: aout_mach_to_elf(&aout),
+            version: 1,
+            entry: aout.entry_point.into(),
+            program_header_offset: ELF_HEADER_SIZE,
+            section_header_offset: 0x00,
+            flags: 0x00,
+            elf_header_size: ELF_HEADER_SIZE as u16,
+            program_header_entry_size: 32,
+            program_header_entry_count: 3,
+            section_header_entry_size: 0,
+            section_header_entry_count: 0,
+            section_header_index_entry: 0,
+        };
+        let b = elf.as_bytes().to_vec();
+
+        let data = &d[AOUT_HEADER_SIZE..];
+
+        Ok([&b, data].concat())
+    } else {
+        Err("Could not parse a.out".to_string())
+    }
 }
 
 fn main() -> std::io::Result<()> {
